d777fa00427e80d84f934a586df00483708b30ce
Leonardo Lopes <69646199+LeonardoDev-df@users.noreply.github.com>
2021-05-04 13:41:38 -0300

    Documentos da Simulação de Projeto
    Tem documentação, imagens e código.

---
+++ Simulação de projeto/script.js
@@ -0,0 +1,26 @@
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(evento){
+  evento.preventDefault();
+  var isvalid = true;
+  if (String(form.nome.value.length) == 0  || form.email.value == "" || form.celular.value == "" || form.msg.value == 0){
+            Swal.fire({
+                icon: 'error',
+                title: 'Verifique os dados e tente novamente!',
+                timer: 19500
+            });
+            isvalid = false;
+  }else{
+         Swal.fire({
+         position: 'center',
+         icon: 'success',
+         title: 'Matriculado com Sucesso!',
+         timer: 5500
+         });	
+  }  
+  console.log(isvalid);     
+});
+
+form.addEventListener('reset', function(){
+      
+});
