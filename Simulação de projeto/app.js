d0af1adb3664c902cb19404d2f3ed36cae00a287
Leonardo Lopes <69646199+LeonardoDev-df@users.noreply.github.com>
2021-05-05 10:32:28 -0300

    Delete Simulação de projeto directory

--- Simulação de projeto/app.js
+++
@@ -1,7 +0,0 @@
-// Função botão voltar para o topo
-    var btn = document.getElementById('back-to-top');
-
-    btn.addEventListener('click', function(){
-        window.scrollTo(0, 0);
-    }, 5000);
-
