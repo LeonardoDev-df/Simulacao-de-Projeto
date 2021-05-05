026136e9603520644bd015a2a4eab99bcc8fadc1
Leonardo Lopes <69646199+LeonardoDev-df@users.noreply.github.com>
2021-05-05 10:34:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Função botão voltar para o topo
+    var btn = document.getElementById('back-to-top');
+
+    btn.addEventListener('click', function(){
+        window.scrollTo(0, 0);
+    }, 5000);
+
