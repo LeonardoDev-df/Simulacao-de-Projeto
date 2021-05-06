738d7f8632e4043a136f7411d31d4107f79ec2c8
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-05-06 13:19:16 -0300

    correção de bug no layout + ajustes na estrutura do código

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 var form = document.querySelector('form');
 
-form.addEventListener('submit', function(evento){
+form.addEventListener('submit', function (evento) {
   evento.preventDefault();
-  var isvalid = true;
-  if (String(form.nome.value.length) == 0  || form.email.value == "" || form.celular.value == "" || form.msg.value == 0){
-            Swal.fire({
-                icon: 'error',
-                title: 'Verifique os dados e tente novamente!',
-                timer: 19500
-            });
-            isvalid = false;
-  }else{
-         Swal.fire({
-         position: 'center',
-         icon: 'success',
-         title: 'Matriculado com Sucesso!',
-         timer: 5500
-         });	
-  }  
-  console.log(isvalid);     
+
+  let isvalid = true;
+
+  if (String(form.nome.value.length) == 0 || form.email.value == "" || form.celular.value == "" || form.msg.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Verifique os dados e tente novamente!',
+      timer: 19500
+    });
+    isvalid = false;
+  } else {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Matriculado com Sucesso!',
+      timer: 5500
+    });
+  }
+
+  console.log(isvalid);
+
 });
 
-form.addEventListener('reset', function(){
-      
+form.addEventListener('reset', function () {
+
 });
